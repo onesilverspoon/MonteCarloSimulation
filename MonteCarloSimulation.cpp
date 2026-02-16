16981aee6bb28d409d2ca8460455b5d929463e44
apostolos ntotis <238523167+onesilverspoon@users.noreply.github.com>
2026-02-16 22:16:58 +0200

    Fixxed variable names to industry standard

--- MonteCarloSimulation.cpp
+++ MonteCarloSimulation.cpp
@@ -22,18 +22,18 @@ double black_scholes_put(double S, double K, double r, double sigma, double T) {
 }
 
 
-void monte_carlo_call_put_price(int num_sims, double Option_price, double Strike_price, double risk_free_per, double sigma, double Time_till_exp, std::mt19937& gen, double& call_seq, double& put_seq) {
-    double Drift = Time_till_exp * (risk_free_per - 0.5 * sigma * sigma);
+void monte_carlo_call_put_price(int num_sims, double S, double K, double r, double sigma, double T, std::mt19937& gen, double& call_seq, double& put_seq) {
+    double Drift = T * (r - 0.5 * sigma * sigma);
     double Op_price_cur1 = 0.0;
     double Op_price_cur2 = 0.0;
-    double Vol_sq = sigma * std::sqrt(Time_till_exp);
+    double Vol_sq = sigma * std::sqrt(T);
     double call_sum = 0.0;
     double put_sum = 0.0;
     double call_sq_sum = 0.0;
     double put_sq_sum = 0.0;
     double call_payoff1, call_payoff2;
     double put_payoff1, put_payoff2;
-    double Discount = std::exp(-risk_free_per * Time_till_exp);
+    double Discount = std::exp(-r * T);
     int half_sims = num_sims / 2;
 
     std::normal_distribution<double> d(0.0, 1.0);
@@ -42,16 +42,16 @@ void monte_carlo_call_put_price(int num_sims, double Option_price, double Strike
     for (int i = 0; i < half_sims; i++) {
         double gauss_bm = d(gen);
         double gauss_bm_antithetic = -gauss_bm;
-        Op_price_cur1 = Option_price * std::exp(Drift + Vol_sq * gauss_bm);
-        Op_price_cur2 = Option_price * std::exp(Drift + Vol_sq * gauss_bm_antithetic);
-        call_payoff1 = std::max(Op_price_cur1 - Strike_price, 0.0);
-        call_payoff2 = std::max(Op_price_cur2 - Strike_price, 0.0);
+        Op_price_cur1 = S * std::exp(Drift + Vol_sq * gauss_bm);
+        Op_price_cur2 = S * std::exp(Drift + Vol_sq * gauss_bm_antithetic);
+        call_payoff1 = std::max(Op_price_cur1 - K, 0.0);
+        call_payoff2 = std::max(Op_price_cur2 - K, 0.0);
 
         call_sum += call_payoff1 + call_payoff2;
         call_sq_sum += call_payoff1 * call_payoff1 + call_payoff2 * call_payoff2;
 
-        put_payoff1 = std::max(Strike_price - Op_price_cur1, 0.0);
-        put_payoff2 = std::max(Strike_price - Op_price_cur2, 0.0);
+        put_payoff1 = std::max(K - Op_price_cur1, 0.0);
+        put_payoff2 = std::max(K - Op_price_cur2, 0.0);
 
         put_sum += put_payoff1 + put_payoff2;
         put_sq_sum += put_payoff1 * put_payoff1 + put_payoff2 * put_payoff2;
@@ -90,10 +90,10 @@ void monte_carlo_call_put_price(int num_sims, double Option_price, double Strike
 
 }
 
-void monte_carlo_call_put_price_paral(int num_sims, double Option_price, double Strike_price, double risk_free_per, double sigma, double Time_till_exp, double& call_paral, double& put_paral) {
-    double Drift = Time_till_exp * (risk_free_per - 0.5 * sigma * sigma);
-    double Vol_sq = sigma * std::sqrt(Time_till_exp);
-    double Discount = std::exp(-risk_free_per * Time_till_exp);
+void monte_carlo_call_put_price_paral(int num_sims, double S, double K, double r, double sigma, double T, double& call_paral, double& put_paral) {
+    double Drift = T * (r - 0.5 * sigma * sigma);
+    double Vol_sq = sigma * std::sqrt(T);
+    double Discount = std::exp(-r * T);
     double call_sum = 0.0;
     double put_sum = 0.0;
     int half_sims = num_sims / 2;
@@ -111,12 +111,12 @@ void monte_carlo_call_put_price_paral(int num_sims, double Option_price, double 
         for (int i = 0; i < half_sims; i++) {
             double gauss_bm = d(gen);
             double gauss_bm_antithetic = -gauss_bm;
-            Op_price_cur1 = Option_price * std::exp(Drift + Vol_sq * gauss_bm);
-            Op_price_cur2 = Option_price * std::exp(Drift + Vol_sq * gauss_bm_antithetic);
-            call_sum += std::max(Op_price_cur1 - Strike_price, 0.0);
-            call_sum += std::max(Op_price_cur2 - Strike_price, 0.0);
-            put_sum += std::max(Strike_price - Op_price_cur1, 0.0);
-            put_sum += std::max(Strike_price - Op_price_cur2, 0.0);
+            Op_price_cur1 = S * std::exp(Drift + Vol_sq * gauss_bm);
+            Op_price_cur2 = S * std::exp(Drift + Vol_sq * gauss_bm_antithetic);
+            call_sum += std::max(Op_price_cur1 - K, 0.0);
+            call_sum += std::max(Op_price_cur2 - K, 0.0);
+            put_sum += std::max(K - Op_price_cur1, 0.0);
+            put_sum += std::max(K - Op_price_cur2, 0.0);
         }
     }
     call_paral = (call_sum / static_cast<double>(num_sims)) * Discount;
@@ -128,22 +128,22 @@ int main()
 {
     int num_sims = 10000000;
     omp_set_num_threads(omp_get_num_procs());
-    double Option_price = 100.0;
-    double Strike_price = 100.0;
-    double risk_free_per = 0.05;
+    double S = 100.0;
+    double K = 100.0;
+    double r = 0.05;
     double sigma = 0.2;
-    double Time_till_exp = 1.0; //in years
+    double T = 1.0; //in years
     std::mt19937 gen(42);
 
     double call_seq, put_seq;
     double call_paral, put_paral;
 
     double start_seq = omp_get_wtime();
-    monte_carlo_call_put_price(num_sims, Option_price, Strike_price, risk_free_per, sigma, Time_till_exp, gen, call_seq, put_seq);
+    monte_carlo_call_put_price(num_sims, S, K, r, sigma, T, gen, call_seq, put_seq);
     double end_seq = omp_get_wtime();
 
     double start_par = omp_get_wtime();
-    monte_carlo_call_put_price_paral(num_sims, Option_price, Strike_price, risk_free_per, sigma, Time_till_exp, call_paral, put_paral);
+    monte_carlo_call_put_price_paral(num_sims, S, K, r, sigma, T, call_paral, put_paral);
     double end_par = omp_get_wtime();
 
     double t_seq = end_seq - start_seq;
@@ -163,11 +163,11 @@ int main()
     std::cout << "Speedup: " << speedup << "x\nEfficiency: " << efficiency << "%" << std::endl;
 
     //black_schole benchmark
-    double BS_call = black_scholes_call(Option_price, Strike_price, risk_free_per, sigma, Time_till_exp);
+    double BS_call = black_scholes_call(S, K, r, sigma, T);
     double abs_error_call = std::abs(call_seq - BS_call);
     double rel_error_call = abs_error_call / BS_call;
 
-    double BS_put = black_scholes_put(Option_price, Strike_price, risk_free_per, sigma, Time_till_exp);
+    double BS_put = black_scholes_put(S, K, r, sigma, T);
     double abs_error_put = std::abs(put_seq - BS_put);
     double rel_error_put = abs_error_put / BS_put;
 
