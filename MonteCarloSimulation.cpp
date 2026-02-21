4adbf87bae74208d375cabfdaa42d3ca38508c7a
apostolos ntotis <238523167+onesilverspoon@users.noreply.github.com>
2026-02-21 23:03:19 +0200

    Refactor seq function and removed printing from inside function to int main()

--- MonteCarloSimulation.cpp
+++ MonteCarloSimulation.cpp
@@ -9,10 +9,30 @@
 struct Monte_carlo_results {
     double call;
     double put;
-    double varience;
-    double ci_low;
-    double ci_high;
-    double str_error;
+
+    //Monte-Carlo stats
+    double call_variance;
+    double put_variance;
+    double call_std_error;
+    double put_std_error;
+
+    double call_ci_low;
+    double call_ci_high;
+    double put_ci_low;
+    double put_ci_high;
+
+    //Control Variance
+    double call_cv;
+    double put_cv;
+    double call_cv_variance;
+    double put_cv_variance;
+    double call_cv_std_error;
+    double put_cv_std_error;
+    double variance_reduction;
+
+    double beta;
+
+    
 };
 
 double norm_cdf(double x) {
@@ -31,7 +51,7 @@ double black_scholes_put(double S, double K, double r, double sigma, double T) {
 }
 
 
-void monte_carlo_call_put_price(int num_sims, double S, double K, double r, double sigma, double T, std::mt19937& gen, double& call_seq, double& put_seq) {
+Monte_carlo_results monte_carlo_call_put_price(int num_sims, double S, double K, double r, double sigma, double T, std::mt19937& gen) {
     double Drift = T * (r - 0.5 * sigma * sigma);
     double Vol_sqrt_T = sigma * std::sqrt(T);
     double Discount = std::exp(-r * T);
@@ -82,8 +102,8 @@ void monte_carlo_call_put_price(int num_sims, double S, double K, double r, doub
     double call_mean = call_sum / num_sims;
     double put_mean = put_sum / num_sims;
     //price estimation
-    call_seq = call_mean * Discount;
-    put_seq = put_mean * Discount;
+    double call_seq = call_mean * Discount;
+    double put_seq = put_mean * Discount;
 
     //var calculation (undiscounted)
     double call_var = (call_sq_sum / num_sims) - call_mean * call_mean;
@@ -102,17 +122,9 @@ void monte_carlo_call_put_price(int num_sims, double S, double K, double r, doub
     double put_ci_low = put_seq - 1.96 * put_se;
     double put_ci_high = put_seq + 1.96 * put_se;
 
-    // Statistics printing
-    std::cout << "Call price        :" << call_seq << std::endl;
-    std::cout << "Standard error    :" << call_se << std::endl;
-    std::cout << "95% CI            :[" << call_ci_low << ", " << call_ci_high << " ]" << std::endl;
-    std::cout << "Put price         :" << put_seq << std::endl;
-    std::cout << "Standard error    :" << put_se << std::endl;
-    std::cout << "95% CI            :[" << put_ci_low << ", " << put_ci_high << " ]" << std::endl;
-    std::cout << "--------------------------------------------------------------" << std::endl;
     //beta calculation
-    double X_mean = call_seq;           // call seq already discounted
-    double Y_mean = Y_sum / num_sims;   //discounted ST mean
+    double X_mean = (call_sum / num_sims) * Discount;           // call seq already discounted??
+    double Y_mean = Y_sum / num_sims;                           //discounted ST mean
     double cov_XY = (cross_sum / num_sims) - X_mean * Y_mean;
     double var_Y = (Y_sq_sum / num_sims) - Y_mean * Y_mean;
     double beta = cov_XY / var_Y;
@@ -120,23 +132,29 @@ void monte_carlo_call_put_price(int num_sims, double S, double K, double r, doub
     //control variate
     double expected_Y = S ;                                     // E[e^{-rT} S_T] = S , risk-neutral measure.
     double call_cv = X_mean + beta * (expected_Y - Y_mean);     // new price
-    std::cout << "CV new price              :" << call_cv << std::endl;
 
     //CV variace
     double var_CV = call_var_MC - beta * beta * var_Y;
     double se_cv = std::sqrt(var_CV / num_sims);
     double reduction = 1.0 - (var_CV / call_var_MC);
 
-    std::cout << "CV Standard error         :" << se_cv << std::endl;
-    std::cout << "MC var                    :" << call_var_MC << std::endl;
-    std::cout << "Varience Reduction        :" << reduction << std::endl;
-
     Monte_carlo_results result;
     result.call = call_seq;
     result.put = put_seq;
-    result.varience = call_var_MC;
-    result.str_error = call_se;
+    result.call_variance = call_var_MC;
+    result.put_variance = put_var_MC;
+    result.call_std_error = call_se;
+    result.put_std_error = put_se;
+    result.call_ci_low = call_ci_low;
+    result.call_ci_high = call_ci_high;
+    result.put_ci_low = put_ci_low;
+    result.put_ci_high = put_ci_high;
+    result.call_cv = call_cv;
+    result.call_cv_std_error = var_CV;
+    result.variance_reduction = reduction;
+    result.beta = beta;
 
+    return result;
 
 }
 
@@ -185,11 +203,11 @@ int main()
     double T = 1.0; //in years
     std::mt19937 gen(42);
 
-    double call_seq, put_seq;
+    //double call_seq, put_seq;
     double call_paral, put_paral;
 
     double start_seq = omp_get_wtime();
-    monte_carlo_call_put_price(num_sims, S, K, r, sigma, T, gen, call_seq, put_seq);
+    Monte_carlo_results res = monte_carlo_call_put_price(num_sims, S, K, r, sigma, T, gen);
     double end_seq = omp_get_wtime();
 
     double start_par = omp_get_wtime();
@@ -208,6 +226,13 @@ int main()
     double speedup = t_seq / t_par;
     double efficiency = (speedup / threads) * 100;
 
+    std::cout << "Call price: " << res.call << "\n";
+    std::cout << "Call SE:    " << res.call_std_error << "\n";
+    std::cout << "Call CI:    [" << res.call_ci_low << ", " << res.call_ci_high << "]\n";
+
+    std::cout << "CV Call:    " << res.call_cv << "\n";
+    std::cout << "Var Red:    " << res.variance_reduction << "\n";
+
  
     std::cout << "--------------------------------------------------------------" << std::endl;
     std::cout << "Call Parallel: " << call_paral << "\nPut Parallel: " << put_paral << std::endl;
@@ -215,11 +240,11 @@ int main()
     std::cout << "--------------------------------------------------------------" << std::endl;
     //black_schole benchmark
     double BS_call = black_scholes_call(S, K, r, sigma, T);
-    double abs_error_call = std::abs(call_seq - BS_call);
+    double abs_error_call = std::abs(res.call - BS_call);
     double rel_error_call = abs_error_call / BS_call;
 
     double BS_put = black_scholes_put(S, K, r, sigma, T);
-    double abs_error_put = std::abs(put_seq - BS_put);
+    double abs_error_put = std::abs(res.put - BS_put);
     double rel_error_put = abs_error_put / BS_put;
 
     std::cout << "--------------------------------------------------------------" << std::endl;
