2d62a92c64111499a89014114db4635aeb645746
apostolos ntotis <238523167+onesilverspoon@users.noreply.github.com>
2026-02-21 23:58:24 +0200

    Added Delta, gamma, vega and cv adjusted

--- MonteCarloSimulation.cpp
+++ MonteCarloSimulation.cpp
@@ -30,7 +30,18 @@ struct Monte_carlo_results {
     double put_cv_std_error;
     double variance_reduction;
 
+    //Greeks
     double beta;
+    double call_delta;
+    double put_delta;
+    double call_gamma;
+    double put_gamma;
+    double call_vega;
+    double put_vega;
+
+    //Greeks CV adjusted
+    double call_delta_cv;
+    double put_delta_cv;
 
     
 };
@@ -65,10 +76,17 @@ Monte_carlo_results monte_carlo_call_put_price(int num_sims, double S, double K,
     double call_sq_sum = 0.0;
     double put_sq_sum = 0.0;
 
-    //beta
+    //beta,delta,gamma,vega, CV accumulators
     double Y_sum = 0.0;
     double Y_sq_sum = 0.0;
     double cross_sum = 0.0;
+    double delta_call_sum = 0.0;
+    double delta_put_sum = 0.0;
+    double gamma_call_sum = 0.0;
+    double gamma_put_sum = 0.0;
+    double vega_call_sum = 0.0;
+    double vega_put_sum = 0.0;
+    double cross_sum_put = 0.0;
 
 
     for (int i = 0; i < half_sims; i++) {
@@ -76,11 +94,34 @@ Monte_carlo_results monte_carlo_call_put_price(int num_sims, double S, double K,
         double gauss_bm_antithetic = -gauss_bm;
         double ST1 = S * std::exp(Drift + Vol_sqrt_T * gauss_bm);
         double ST2 = S * std::exp(Drift + Vol_sqrt_T * gauss_bm_antithetic);
+        double Z1 = gauss_bm;
+        double Z2 = gauss_bm_antithetic;
 
         double call_payoff1 = std::max(ST1 - K, 0.0);
         double call_payoff2 = std::max(ST2 - K, 0.0);
         double put_payoff1 = std::max(K - ST1, 0.0);
         double put_payoff2 = std::max(K - ST2, 0.0);
+
+        //Delta
+        if (ST1 > K)delta_call_sum += ST1 / S;
+        if (ST2 > K)delta_call_sum += ST2 / S;
+        if (ST1 < K)delta_call_sum -= ST1 / S;
+        if (ST2 < K)delta_call_sum -= ST2 / S;
+
+        //Gamma
+        double common1 = (Z1 / (S * sigma * std::sqrt(T)));
+        double common2 = (Z2 / (S * sigma * std::sqrt(T)));
+        gamma_call_sum += std::max(ST1 - K, 0.0) * common1;
+        gamma_call_sum += std::max(ST2 - K, 0.0) * common2;
+        gamma_put_sum += std::max(K - ST1, 0.0) * common1;
+        gamma_put_sum += std::max(K - ST2, 0.0) * common2;
+
+        //Vega
+        vega_call_sum += std::max(ST1 - K, 0.0) * (Z1 * std::sqrt(T) - sigma * T);
+        vega_call_sum += std::max(ST2 - K, 0.0) * (Z2 * std::sqrt(T) - sigma * T);
+        vega_put_sum += std::max(K - ST1, 0.0) * (Z1 * std::sqrt(T) - sigma * T);
+        vega_put_sum += std::max(K - ST2, 0.0) * (Z2 * std::sqrt(T) - sigma * T);
+
         //call stats
         call_sum += call_payoff1 + call_payoff2;
         call_sq_sum += call_payoff1 * call_payoff1 + call_payoff2 * call_payoff2;
@@ -97,6 +138,10 @@ Monte_carlo_results monte_carlo_call_put_price(int num_sims, double S, double K,
         Y_sum += discounted_ST1 + discounted_ST2;
         Y_sq_sum += discounted_ST1 * discounted_ST1 + discounted_ST2 * discounted_ST2;
         cross_sum += discounted_call1 * discounted_ST1 + discounted_call2 * discounted_ST2;
+
+        double discounted_put1 = put_payoff1 * Discount;
+        double discounted_put2 = put_payoff2 * Discount;
+        cross_sum_put += discounted_put1 * discounted_ST1 + discounted_put2 * discounted_ST2;
     }
     //mean calculation
     double call_mean = call_sum / num_sims;
@@ -123,20 +168,36 @@ Monte_carlo_results monte_carlo_call_put_price(int num_sims, double S, double K,
     double put_ci_high = put_seq + 1.96 * put_se;
 
     //beta calculation
-    double X_mean = (call_sum / num_sims) * Discount;           // call seq already discounted??
-    double Y_mean = Y_sum / num_sims;                           //discounted ST mean
-    double cov_XY = (cross_sum / num_sims) - X_mean * Y_mean;
+    double X_mean = (call_sum / num_sims) * Discount;           // not already discounted
+    double Y_mean = Y_sum / num_sims;                           // discounted ST mean
+    double cov_XY = (cross_sum / num_sims) - X_mean * Y_mean;   // already discounted
     double var_Y = (Y_sq_sum / num_sims) - Y_mean * Y_mean;
-    double beta = cov_XY / var_Y;
+    double beta = (var_Y > 1e-12) ? (cov_XY / var_Y) : 0.0;     // avoid div by 0
+    double call_delta = Discount * (delta_call_sum / num_sims);
+    double put_delta = Discount * (delta_put_sum / num_sims);
+
+    double call_gamma = Discount * (gamma_call_sum / num_sims);
+    double put_gamma = Discount * (gamma_put_sum / num_sims);
+
+    double call_vega = Discount * (vega_call_sum / num_sims);
+    double put_vega = Discount * (vega_put_sum / num_sims);
     
     //control variate
     double expected_Y = S ;                                     // E[e^{-rT} S_T] = S , risk-neutral measure.
     double call_cv = X_mean + beta * (expected_Y - Y_mean);     // new price
 
     //CV variace
-    double var_CV = call_var_MC - beta * beta * var_Y;
-    double se_cv = std::sqrt(var_CV / num_sims);
-    double reduction = 1.0 - (var_CV / call_var_MC);
+    double var_cv = call_var_MC + beta * beta * var_Y - 2.0 * beta * cov_XY;
+    double se_cv = std::sqrt(var_cv / num_sims);
+    double reduction = 1.0 - (var_cv / call_var_MC);
+    double X_put_mean = (put_sum / num_sims) * Discount;
+    double cov_put_Y = (cross_sum_put / num_sims) - X_put_mean * Y_mean;
+    double beta_put = (var_Y > 1e-12) ? cov_put_Y / var_Y : 0.0;
+    double put_cv = X_put_mean + beta_put * (expected_Y - Y_mean);
+    double var_put_CV = put_var_MC + beta_put * beta_put * var_Y - 2.0 * beta_put * cov_put_Y;
+    double put_se_cv = std::sqrt(var_put_CV / num_sims);
+
+    
 
     Monte_carlo_results result;
     result.call = call_seq;
@@ -150,9 +211,19 @@ Monte_carlo_results monte_carlo_call_put_price(int num_sims, double S, double K,
     result.put_ci_low = put_ci_low;
     result.put_ci_high = put_ci_high;
     result.call_cv = call_cv;
-    result.call_cv_std_error = var_CV;
+    result.call_cv_variance = var_cv;
+    result.call_cv_std_error = se_cv;
     result.variance_reduction = reduction;
     result.beta = beta;
+    result.call_delta = call_delta;
+    result.put_delta = put_delta;
+    result.call_gamma = call_gamma;
+    result.put_gamma = put_gamma;
+    result.call_vega = call_vega;
+    result.put_vega = put_vega;
+    result.put_cv = put_cv;
+    result.put_cv_variance = var_put_CV;
+    result.put_cv_std_error = put_se_cv;
 
     return result;
 
@@ -226,17 +297,29 @@ int main()
     double speedup = t_seq / t_par;
     double efficiency = (speedup / threads) * 100;
 
-    std::cout << "Call price: " << res.call << "\n";
-    std::cout << "Call SE:    " << res.call_std_error << "\n";
-    std::cout << "Call CI:    [" << res.call_ci_low << ", " << res.call_ci_high << "]\n";
-
-    std::cout << "CV Call:    " << res.call_cv << "\n";
-    std::cout << "Var Red:    " << res.variance_reduction << "\n";
+    std::cout << "Call price        :" << res.call << std::endl;
+    std::cout << "Call Var          :" << res.call_variance << std::endl;
+    std::cout << "Call SE           :" << res.call_std_error << std::endl;
+    std::cout << "Call CI           :[" << res.call_ci_low << ", " << res.call_ci_high << "]" << std::endl;
+    std::cout << "Puts price        :" << res.put << std::endl;
+    std::cout << "Puts Var          :" << res.put_variance << std::endl;
+    std::cout << "Puts SE           :" << res.put_std_error << std::endl;
+    std::cout << "Puts CI           :[" << res.put_ci_low << ", " << res.put_ci_high << "]" << std::endl;
+    std::cout << "--------------------------------------------------------------" << std::endl;
+    std::cout << "CV Call           :" << res.call_cv << std::endl;
+    std::cout << "CV Call Var       :" << res.call_cv_variance << std::endl;
+    std::cout << "CV Call SE        :" << res.call_cv_std_error << std::endl;
+    std::cout << "CV Puts           :" << res.put_cv << std::endl;
+    std::cout << "CV Puts Var       :" << res.call_cv_variance << std::endl;
+    std::cout << "CV Puts SE        :" << res.call_cv_std_error << std::endl;
+    std::cout << "Var Red           :" << res.variance_reduction << std::endl;
 
  
     std::cout << "--------------------------------------------------------------" << std::endl;
-    std::cout << "Call Parallel: " << call_paral << "\nPut Parallel: " << put_paral << std::endl;
-    std::cout << "Speedup: " << speedup << "x\nEfficiency: " << efficiency << "%" << std::endl;
+    std::cout << "Call Parallel     :" << call_paral << std::endl;
+    std::cout << "Puts Parallel     :" << put_paral << std::endl;
+    std::cout << "Speedup           :" << speedup << std::endl;
+    std::cout << "Efficiency        :" << efficiency << "%" << std::endl;
     std::cout << "--------------------------------------------------------------" << std::endl;
     //black_schole benchmark
     double BS_call = black_scholes_call(S, K, r, sigma, T);
@@ -255,6 +338,9 @@ int main()
     std::cout << "Black-Scholes puts:" << BS_put << std::endl;
     std::cout << "Absolute error    :" << abs_error_put << std::endl;
     std::cout << "Relative error    :" << rel_error_put << std::endl;
+    std::cout << "--------------------------------------------------------------" << std::endl;
+    std::cout << "Greeks" << std::endl;
+    std::cout << "Beta              :" << res.beta << std::endl;
 
     return 0;
 }
